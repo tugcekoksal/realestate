f28b3e3389dea4868aa3d68fe7b39975d5803552
tugcekoksal <tugcekoksal35@gmail.com>
2023-09-18 19:30:46 +0200

    change

--- pages/search.js
+++ pages/search.js
@@ -33,7 +33,7 @@ const Search = ({ properties }) => {
       </Text>
       <Flex flexWrap={"wrap"}>
         {properties.map((property) => (
-          <Property property={property} />
+          <Property property={property} key={property.id} />
         ))}
       </Flex>
       {properties.length === 0 && (
